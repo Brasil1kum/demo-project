5d2ca2db68afd7c2676c9dedb626f8ae31041540
Brasil1kum <adrian.mehmke@dynabase.de>
2024-08-12 14:38:18 +0200

    init

---
+++ demo-monorepo/apps/demo-monorepo/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule, LoginComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'demo-monorepo';
+}
